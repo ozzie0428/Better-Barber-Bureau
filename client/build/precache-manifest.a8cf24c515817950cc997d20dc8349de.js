5eece23a14429d18135beef6a853ae6f6cb64206
Ozzie Cunningham <oswald3.cunningham@gmail.com>
2020-01-16 10:54:01 -0500

    intial commut

---
+++ client/build/precache-manifest.a8cf24c515817950cc997d20dc8349de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d20b264f70b18a87b2a4900c9275cb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc3b752ef85e1df2f2d2",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "bc3b752ef85e1df2f2d2",
+    "url": "/static/js/main.0e7e3b18.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
