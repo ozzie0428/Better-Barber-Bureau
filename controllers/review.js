443d0e776b40a4645a500ad1d47f0c82ea8817f1
Ozzie Cunningham <oswald3.cunningham@gmail.com>
2020-01-16 22:13:18 -0500

    modified review controllers

--- controllers/review.js
+++ controllers/review.js
@@ -1,1 +1,110 @@
-gi
+const express = require("express");
+
+
+const reviewApi = require("../models/review.js");
+
+
+
+const ReviewRouter = express.Router();
+
+
+
+ReviewRouter.get("/", (req, res) => {
+  reviewApi
+    .getAllReviews()
+    .then(allReviews => {
+      res.render("review/allReviews", { allReviews });
+    })
+    .catch(error => {
+      console.log(error);
+      res.send(error);
+    });
+});
+
+ReviewRouter.get("/new", (req, res) => {
+  res.render("review/createReview");
+});
+
+ReviewRouter.get("/edit/:id", (req, res) => {
+  const reviewId = req.params.id;
+
+  reviewApi
+    .getReviewById(reviewId)
+    .then(review => {
+      res.render("review/editReview", { review });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+ReviewRouter.get("/:id", (req, res) => {
+  const reviewId = req.params.id;
+
+  reviewApi
+    .getReviewById(reviewId)
+    .then(review => {
+      res.render("review/singleReview", { review });
+    })
+    .catch(error => {
+      console.log(error);
+      res.send(error);
+    });
+});
+
+ReviewRouter.post("/", (req, res) => {
+  const newReview = req.body;
+
+  reviewApi
+    .createReview(newReview)
+    .then(createdReview => {
+      res.redirect("/review");
+    })
+    .catch(error => {
+      console.log(error);
+      res.send(error);
+    });
+});
+
+ReviewRouter.put("/:id", (req, res) => {
+  reviewApi
+    .updateReview(req.params.id, req.body)
+    .then(() => {
+      res.redirect(`/review/${req.params.id}`);
+    })
+    .catch(error => {
+      console.log(error);
+      res.send(error);
+    });
+});
+
+ReviewRouter.delete("/:id", (req, res) => {
+  const reviewId = req.params.id;
+  reviewApi
+    .deleteReview(reviewId)
+    .then(() => {
+      res.redirect("/review");
+    })
+    .catch(error => {
+      console.log(error);
+      res.send(error);
+    });
+});
+
+ReviewRouter.get("/byName/:name", (req, res) => {
+  const reviewName = req.params.reviewName;
+
+  reviewApi
+    .getReviewByName(reviewName)
+    .then(reviews => {
+      res.json(reviews);
+    })
+    .catch(error => {
+      console.log(error);
+      res.send(error);
+    });
+});
+
+module.exports = {
+  ReviewRouter
+};
