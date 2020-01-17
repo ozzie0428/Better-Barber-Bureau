0db9f324aaff0f802dec04177a539db8bf10a1a0
Ozzie Cunningham <oswald3.cunningham@gmail.com>
2020-01-17 13:46:26 -0500

    create equipment component

---
+++ client/src/components/Equipment.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Equipment extends Component {
+    render() {
+        return (
+            <div>
+                <h1>HELLO FROM EQUIPMET </h1>
+            </div>
+        )
+    }
+}
