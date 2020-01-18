9d01486e912fb36e01bfe389faced0e900660ae9
Ozzie Cunningham <oswald3.cunningham@gmail.com>
2020-01-17 20:00:33 -0500

    create Single barber component

---
+++ client/src/components/SingleBarber.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SingleBarber extends Component {
+    render() {
+        return (
+            <div>
+                <h1>HELLO FROM SINGLE BARBER </h1>
+            </div>
+        )
+    }
+}
