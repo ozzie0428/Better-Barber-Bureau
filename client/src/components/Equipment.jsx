a1203842ec029371c5c7b31f4d53fb44ce7a494c
Ozzie Cunningham <oswald3.cunningham@gmail.com>
2020-01-18 14:56:23 -0500

    take away data imgs

--- client/src/components/Equipment.jsx
+++ client/src/components/Equipment.jsx
@@ -1,54 +1,20 @@
-import React, { Component } from 'react'
-import axios from "axios";
-// import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import equipmentData from '../equipmentData'
+import EquipmentItem from './EquipmentItem'
 
 export default class Equipment extends Component {
-    state = {
-        newEquipmetName: "",
-         image: "",
-         description: "",
-         price: 0
-    }
-    componentDidMount() {
-        this.updateEquipmetPage();
-      }
-    
-      updateEquipmetPage = () => {
-        axios.get("/api/equipmet").then(res => {
-          console.log("res.data", res.data);
-          this.setState({ equipmetList: res.data });
-        });
-      };
-      createEquipmet = () => {
-        const newEquipmet = {
-          name: this.state.newEquipmetName,
-          image: this.state.image,
-          price: this.state.price
-        };
-        axios.post("/api/equipmet", newEquipmet).then(() => {
-          const newState = { ...this.state };
-          newState.newEquipmetName = "";
-          newState.price = "";
-          this.setState(newState);
-          this.updateEquipmetPage();
-        });
-      };
-    
-      handleChange = event => {
-        const inputValue = event.target.value;
-        this.setState({ [event.target.name]: inputValue });
-      };
-    
-
-
-
-
-
-    render() {
-        return (
-            <div>
-                <h1>HELLO FROM EQUIPMET </h1>
-            </div>
-        )
-    }
+	render() {
+		console.log(equipmentData);
+		return (
+			<div className="equipment">
+				<h1>Shopping</h1>
+                <div className="equipment-container">
+				{equipmentData.map((equipmentItem, i) => {
+					
+					return <EquipmentItem equipmentItem={equipmentItem} key={i} />;
+				})}
+                </div>
+			</div>
+		);
+	}
 }
--- client/src/equipmentData.jsx
+++ client/src/equipmentData.jsx
@@ -3,7 +3,7 @@ export default [
       name: "Andis 17150 Pro Foil Lithium Titanium Foil Shaver, Cord/ Cordless ",
       price: 16.75,
       description: `Staggered-head shaver blades for a closer shave, Lithium-ion battery provides maximum power.Ideal for finishing stubble and fade-style haircuts, for added versatility.`,
-      image: <img src="https://images-na.ssl-images-amazon.com/images/I/61Ju-rjuZxL._SL1380_.jpg" alt="clippers"/>
+      // image: <img src="https://images-na.ssl-images-amazon.com/images/I/61Ju-rjuZxL._SL1380_.jpg" alt="clippers"/>
     }, {
       name: "ANDIS Professional Barber Combo - CL-66325",
       price: 95.75,
@@ -12,12 +12,12 @@ export default [
       Sleek Housing is Lighter & Fits Comfortablyin Your Hand
       Smooth Running Motor Runs Quiet & Cool
       Lever Adjusts Clipper Blade from Fine toCoarse (#000 to #1)`,
-      image: <img src="https://images-na.ssl-images-amazon.com/images/I/61QZSly2aNL._SL1000_.jpg" alt="clippers"/>
+      // image: <img src="https://images-na.ssl-images-amazon.com/images/I/61QZSly2aNL._SL1000_.jpg" alt="clippers"/>
     }, {
       name: "BaBylissPRO Goldfx Outlining Trimmer",
       price: 164.75,
       description: "THE BEST OUT, GOLDEN CLIPPERS",
-      image: <img src="https://images-na.ssl-images-amazon.com/images/I/51eIdfUsoCL._SL1024_.jpg" alt="clippers"/>
+      // image: <img src="https://images-na.ssl-images-amazon.com/images/I/51eIdfUsoCL._SL1024_.jpg" alt="clippers"/>
     }, {
       name: "King Midas Barber Apron Unisex Hair Stylist Apron (Red)",
       price: 28.99,
@@ -26,11 +26,11 @@ export default [
       Full coverage for extra protection - Protect your clothes and enjoy utility pockets and a unique unisex style ideal for barbers, hair stylists, colorists, beauty salon, barber shop, hair salon, hair cutting, hair styling.
       Signature King Midas Cape style- The unisex and adjustable style makes this perfect for any barber, or hair stylist.
       Free Shipping: The King Midas professional apron is quality at an affordable price. Along with free shipping makes this a steal, so grab yours now!`,
-      image: <img src="https://images-na.ssl-images-amazon.com/images/I/718VM9E9GGL._SL1500_.jpg" alt="clippers"/>
+      // image: <img src="https://images-na.ssl-images-amazon.com/images/I/718VM9E9GGL._SL1500_.jpg" alt="clippers"/>
         }, {
       name: "2 Packs Salon Barber Cape with professional Haircut Salon Nylon Cape plus a Carbon Comb",
       price: 16.75,
       description: `THIS WATERPROOF BARBER CAPES INCLUDES: - 2 x hair cutting capes, 1 x Carbon Comb. If You looking for a haridressing waterproof capes that Offer You Personal Care, Without Costing You a Fortune Buying Each Piece Individually, Then Look No Further, Because It Brings You the Personal Care barber salon capes solution of the best!`,
-          image: <img src="https://images-na.ssl-images-amazon.com/images/I/717Cm8SegOL._SL1500_.jpg" alt="clippers"/>
+          // image: <img src="https://images-na.ssl-images-amazon.com/images/I/717Cm8SegOL._SL1500_.jpg" alt="clippers"/>
     }
   ];
