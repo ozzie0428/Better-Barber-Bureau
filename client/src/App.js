ac48af8cc411a03a51752e54b44aeb9bd77f4556
Ozzie Cunningham <oswald3.cunningham@gmail.com>
2020-01-18 23:37:12 -0500

    import singel barber

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
 import Home from './components/Home'
 import Barber from './components/Barber'
+import SingleBarber from "./components/SingleBarber";
 import Review from './components/Review'
 import Equipment from './components/Equipment'
 
@@ -11,19 +12,38 @@ export default class App extends Component {
     return (
       <div>
         <div className="nav-bar">
-      <nav className="nav-list">
-          <ul>
-            <li>Barbers</li>
-            <li>Reviews</li>
-            <li>Euipment Store</li>
-          </ul>
-        </nav>
+      
+          
+            <h3>Barbers</h3>
+            <h3>Reviews</h3>
+            <h3>Euipment Store</h3>
+          
+      
+        </div>
+        <div
+          // style={{
+          //   display: "flex",
+          //   justifyContent: "center",
+          //   alignContent: "center"
+          // }}
+        >
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3TQF-2FsOVYAxIDYEXjwGppI8VNBLOH2MxPxvuUzvvB2mN1xvEg&s"
+            alt=""
+          /> <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3TQF-2FsOVYAxIDYEXjwGppI8VNBLOH2MxPxvuUzvvB2mN1xvEg&s"
+          alt=""
+        />
+        <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3TQF-2FsOVYAxIDYEXjwGppI8VNBLOH2MxPxvuUzvvB2mN1xvEg&s"
+            alt=""
+          />
         </div>
         <Router>
         <Switch>
           <Route exact path= "/" component={Home} />
           <Route exact path= "/api/barber" component={Barber} />
-          {/* <Route exact path= "/api/barber/:barberId" component={Barber} /> */}
+          <Route exact path= "/api/barber/:barberId" component={SingleBarber} />
           <Route exact path= "/api/review" component={Review} />
           <Route exact path= "/api/equipment" component={Equipment} />
         </Switch>
