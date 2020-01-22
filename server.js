a9c39721c8d5d0ebfed151f5d6a0e18f66df9521
Ozzie Cunningham <oswald3.cunningham@gmail.com>
2020-01-22 15:21:02 -0500

    change up route path

--- server.js
+++ server.js
@@ -13,13 +13,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static(__dirname + "/client/build/"));
-app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/client/build/index.html");
-});
 
 app.use("/api/equipment", EquipmentRouter);
 app.use("/api/barber", BarberRouter);
 app.use("/api/review", ReviewRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/client/build/index.html");
+});
 
 const PORT = process.env.PORT || 3001;
 const connectionString =
